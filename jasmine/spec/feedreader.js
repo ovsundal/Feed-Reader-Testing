ed58da1460b0dc7afae74ab73c7a72fc8c365de9
ovsundal <osundal83@gmail.com>
2017-08-08 13:10:26 +0200

    add test to validate that menu is hidden on page load

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -14,45 +14,47 @@ $(function() {
     * feeds definitions, the allFeeds variable in our application.
     */
     describe('RSS Feeds', function() {
-        /* This is our first test - it tests to make sure that the
+         /* This is our first test - it tests to make sure that the
          * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
+         * empty.
          */
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
+        /* Checks that each feed has a url defined,
+        and it is not an empty string
          */
-        it('each feed has a non-empty url', function () {
+        it('each feed has a non-empty url defined', function () {
             allFeeds.forEach(function (item) {
-                debugger;
                expect(item.url).toBeDefined();
                expect(item.url).not.toBe("")
             });
         });
 
-
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
+        /* Checks that each feed has a name defined,
+        and it is not an empty string
          */
+        it('each feed has a non-empty name defined', function () {
+            allFeeds.forEach(function (item) {
+                expect(item.name).toBeDefined();
+                expect(item.name).not.toBe("")
+            });
+        });
+
     });
 
+    describe('The menu', function () {
 
-    /* TODO: Write a new test suite named "The menu" */
+        // Checks that the menu is hidden on page load
+        it('should be hidden by default', function () {
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+        });
 
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         */
+
+    });
+
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
